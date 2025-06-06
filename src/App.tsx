e3fe3a7a6db7d56395648e158b9b3b8baf066143
Zidan Bian <bianzidan@gmail.com>
2025-06-07 00:15:16 +1000

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
-import Series from "./pages/Series";
-import Movies from "./pages/Movies";
 import NotFound from "./pages/NotFound";
+
+// testing lazy load for 1 second delay
+const Movies = React.lazy(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return import("./pages/Movies");
+});
+
+const Series = React.lazy(() => import("./pages/Series"));
+
+const Loading = () => (
+  <div className="loading" data-testid="loading">
+    ......loading......
+  </div>
+);
 
 const App: React.FC = () => {
   return (
@@ -14,8 +25,24 @@ const App: React.FC = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/series" element={<Series />} />
-        <Route path="/movies" element={<Movies />} />
+
+        <Route
+          path="/movies"
+          element={
+            <Suspense fallback={Loading()}>
+              <Movies />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Series"
+          element={
+            <Suspense fallback={Loading()}>
+              <Series />
+            </Suspense>
+          }
+        />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
